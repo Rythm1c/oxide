52345bdb3a3f8587170023b305bf3e9df9b3bd41
Rythm1c <vickkaniu0@gmail.com>
2026-05-09 01:15:04 +0300

    minor change

--- math/src/vec2.rs
+++ math/src/vec2.rs
@@ -1,18 +1,17 @@
-
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Vec2 {
     pub x: f32,
     pub y: f32,
 }
 
-pub fn vec2(_x: f32, _y: f32) -> Vec2 {
-    Vec2 { x: _x, y: _y }
-}
-
 impl Vec2 {
     pub const ZERO: Self = Self { x: 0.0, y: 0.0 };
 
     pub const ONE: Self = Self { x: 1.0, y: 1.0 };
+
+    pub fn new(x: f32, y: f32) -> Vec2 {
+        Vec2 { x, y }
+    }
 
     pub fn from(a: &[f32; 2]) -> Self {
         Self { x: a[0], y: a[1] }
